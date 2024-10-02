ef7d27a0baa1962debe61e9ffab2b6ac986c96ae
dapras2000 <dapras.solo@gmail.com>
2024-10-02 15:58:07 +0700

    add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            InitSeeder::class,
+            // Seeder lainnya...
+        ]);
     }
 }
